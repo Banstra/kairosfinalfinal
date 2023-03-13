813c6fbfbd899b6d508cbcce9c4b65e33340e08f
dozopravka <dozopravka@example.com>
2023-03-13 20:19:20 +0300

    kairos

---
+++ app/src/main/java/com/example/kotocar/dizain.java
@@ -0,0 +1,14 @@
+package com.example.kotocar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class dizain extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dizain);
+    }
+}
---
+++ app/src/main/java/com/example/kotocar/siz.java
@@ -0,0 +1,14 @@
+package com.example.kotocar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class siz extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_siz);
+    }
+}
